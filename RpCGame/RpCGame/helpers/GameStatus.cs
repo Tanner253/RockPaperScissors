f7c1e3501e93c426b054ba1098f76d508ec44aee
Tanner Percival <percivaltanner@gmail.com>
2020-11-12 17:01:03 -0800

    Rock paper scissors finished

---
+++ RpCGame/RpCGame/helpers/GameStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpCGame.helpers
+{
+    public enum GameStatus
+    {
+        Victory, Loss, Draw
+    }
+}
---
+++ RpCGame/RpCGame/helpers/Hand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpCGame.helpers
+{
+    public class Hand
+    {
+        public OptionsRPS Selection { get; set; }
+        public OptionsRPS WinsAgainst { get; set; }
+        public OptionsRPS LosesAgainst { get; set; }
+        public string Image { get; set; }
+
+        public GameStatus PlayAgainst(Hand opponentHand)
+        {
+            if(Selection == opponentHand.Selection)
+            {
+                return GameStatus.Draw;
+            }
+            if(LosesAgainst == opponentHand.Selection)
+            {
+                return GameStatus.Loss;
+            }
+            
+                return GameStatus.Victory;
+            
+        }
+    }
+}
